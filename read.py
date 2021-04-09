3139c5965599fd2be989f7f5a341967cb32e9c53
RyanLin <Willie003652@gmail.com>
2021-04-09 18:40:46 +0800

    list comprehension

--- read.py
+++ read.py
@@ -24,3 +24,26 @@ for d in data:
 print('一共有', len(new) ,'筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+# 篩選 good字串
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d) #good.append(d) 原封不動d裝含good字串裡面 ,如果是good.append(1) 遇到good都會改裝 1
+# print('一共有', len(good) ,'筆留言提到good')
+
+# 快寫法
+# 第一個d = append的d
+#			運算			  變數		清單				篩選條件
+# out = [(number-1) for number in reference if number %2 == 0]
+good = [d for d in data if 'good' in d]
+# print(good)
+
+# bad in d True or false
+bad = ['bad' in d for d in data]
+print(bad)
+
+# bad = []
+# for d in data:
+# 	bad.append('bad' in d)
+# count_1 = 0
